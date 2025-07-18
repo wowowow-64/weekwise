db8cf56bbe888a9291a65e0b819f207c60db5948
Brenda Clark <bcclark11@gmail.com>
2025-07-18 15:26:04 +0000

    Still.........

--- src/app/planner/layout.tsx
+++ src/app/planner/layout.tsx
@@ -26,11 +26,13 @@ export default function PlannerLayout({
   }
 
   return (
-    <div className="flex min-h-screen w-full flex-col">
-      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <Header />
-        {children}
-      </main>
-    </div>
+    <TaskProvider>
+      <div className="flex min-h-screen w-full flex-col">
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          <Header />
+          {children}
+        </main>
+      </div>
+    </TaskProvider>
   );
 }
